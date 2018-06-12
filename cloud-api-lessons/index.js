993a8089dd79800887cd0bda740334b12845df3e
Mehdi Bouheddi <mbouheddi@mbouheddi-ltm.internal.salesforce.com>
2018-06-12 16:58:20 +0200

    new version

--- cloud-api-lessons/index.js
+++ cloud-api-lessons/index.js
@@ -17,32 +17,57 @@ const url = "https://api-http.littlebitscloud.cc/v2/devices/243c201f8634/input";
 const token =
   "0818ce2545bb60e2672e5a18a9d7413ecc8534da02785bee59879c0789197143";
 
-// const options = { headers: { Authorization: token } };
-
-// const es = new EventSource(url, options);
-
-// time to wait in ms
 const delay = 15000;
 
 let lastHit = new Date().getTime();
 
-const percent = JSON.parse(event.data).percent;
-const elapsed = new Date().getTime() - lastHit;
-// be sure to wait a little
-if (percent !== undefined && elapsed > delay && percent > 3) {
-  // send data to sales force
-  console.log('sending request. Poids_tonnes__c = ',percent); 
-  lastHit = new Date().getTime();
-  
-  fetch('https://arcane-wave-26677.herokuapp.com/', { 
-    method: 'POST',
-    body: JSON.stringify({"Poids_tonnes__c":percent,"username":"romain"}),
-    headers: {'Content-Type':'application/json'},
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-}
+app.use(route.post('/', function *() {
+
+  console.log('received POST: %j', this.request.body)
+
+  if (this.request.body && this.request.body.type) {
+    handleCloudbitEvent(this.request.body)
+  }
+
+  this.body = 'OK'
+
+}))
+
+
 
 app.listen(port)
 console.log('App booted on port %d', port)
+
+
+
+// Helpers
+
+function handleCloudbitEvent(event) {
+  console.log('event: ',event);
+  lastHit = new Date().getTime();
+  switch (event.type) {
+    case 'amplitude':
+      // Do whatever you want with the amplitde
+      console.log('inside switch case, event.payload: ',event.payload)
+      console.log('cloudBit input received: %d%', event.payload.percent)
+      const percent = JSON.parse(event.data).percent;
+      const elapsed = new Date().getTime() - lastHit;
+      if (percent !== undefined && elapsed > delay && percent > 3) {
+        fetch('https://arcane-wave-26677.herokuapp.com/', { 
+          method: 'POST',
+          body: JSON.stringify({"Poids_tonnes__c":event.payload.percent,"username":"romain"}),
+          headers: {'Content-Type':'application/json'},
+        })
+          .then(res => res.json())
+          .then(json => console.log("json", json))
+          .catch(err => console.error("err", err));
+      }
+      break
+    case 'connectionChange':
+      // One day, cloudBits will emit this event too, but not yet.
+      break
+    default:
+      console.warn('cloudBit sent an unexpected event: %j', event)
+      break
+  }
+}
